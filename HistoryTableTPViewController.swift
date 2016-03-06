d3c215a45df1f45bebd7cda3401333492ce8a89a
Pablo Diaz Mateusgit config --global user.email “pabloso92@yahoo.com <pabloso92@yahoo.com>
2016-03-06 07:04:50 -0600

    UI changes

--- HistoryTableTPViewController.swift
+++ HistoryTableTPViewController.swift
@@ -13,6 +13,11 @@ class HistoryTableTPViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        UIToolbar.appearance().barTintColor = UIColor(red: 80.0/255.0, green: 210.0/255.0, blue: 194.0/255.0, alpha: 1.0)
+        
+        UINavigationBar.appearance().tintColor = UIColor.whiteColor()
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
+        self.navigationController!.navigationBar.barTintColor = UIColor(red: 72.0/255.0, green: 190.0/255.0, blue: 176.0/255.0, alpha: 1.0)
         // Do any additional setup after loading the view.
     }
 
